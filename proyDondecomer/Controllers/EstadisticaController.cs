103f015440211ed240e4fb50e96efd16e9fa3888
romav <romariovargasj@hotmail.com>
2015-12-17 03:23:29 -0500

    No se logro hacer con linq, esta comentado lo del sql

--- proyDondecomer/Controllers/EstadisticaController.cs
+++ proyDondecomer/Controllers/EstadisticaController.cs
@@ -3,25 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using proyDondecomer.Models;
+using System.Web.Http;
 
 namespace proyDondecomer.Controllers
 {
-    public class EstadisticaController : Controller
+    public class EstadisticaController : ApiController
     {
         //
         // GET: /Estadistica/
+        private dondeComerEntities db = new dondeComerEntities();
 
-        public ActionResult Index()
-        {
-            return View();
+        public EstadisticaController() {
+            db.Configuration.ProxyCreationEnabled = false;
         }
+        
+        //Top 5 de menus más consumidos
+        public IEnumerable<LikeProducto> GetEstadistica()
+        {
+
+
+            var lista = db.LikeProducto.GroupBy(i => i.productoID)
+                        .Select(g => new { Count = g.Count() });
+
+
+
+                
+               /* from l in db.LikeProducto
+                        .GroupBy(l => l.productoID)
+                        select new{
+                            
+                        };*/
+            
+            /*var like = db.LikeProducto.ToList();
+            return like.AsEnumerable();*/
+            return lista as IEnumerable<LikeProducto>;
+        }
+
+        /*
+         * LO DE SQL SERVER
+         * 
+         select top 5  m.nombre as 'NombreMenu', COUNT(*) as 'CantidadLike' from LikeProducto l inner join Menu m
+on l.productoID = m.menuID
+group by productoID, m.nombre
+order by CantidadLike desc
+         */
+
+
+        /*
+        public IEnumerable<Menu> GetTopMenu()
+        {
+             int total;
+
+             var lista = from l in db.LikeProducto
+                         group l by l.productoID into g
+                         select new 
+                         {
+                            Menu = g.Key, total =  g.Count()
+                         };
+           
+            var menu = db.Menu.ToList();
+            return menu.AsEnumerable();
+        }*/
+
 
         //Top 5 de restaurantes por frecuencia de personas
         //Top 5 de restaurantes con más likes
-        //Top 5 de menus más consumidos
+        
         //El menu más consumido por mes
         //El restaurante más visitado por mes
         //
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
 
     }
 }
